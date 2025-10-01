a98bd3dd6a9ff413e002a6ce9256771559160b2d
Vinicio Chaves <47008512+viniciochavess@users.noreply.github.com>
2025-10-01 17:42:33 -0300

    aula 2: 19:30

--- frontend-gestao-marketplace/src/app/app.routes.ts
+++ frontend-gestao-marketplace/src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Layout } from './pages/layout/layout';
+import { Products } from './pages/products/products';
+import { NewProduct } from './pages/new-product/new-product';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path:"",
+    component: Layout,
+    children:[
+      {
+        path: 'products',
+        component: Products
+      },
+      {
+        path: 'new-product',
+        component: NewProduct
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
